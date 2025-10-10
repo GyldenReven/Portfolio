80de758ae9b58e70ecab9c8c3a06028a91e4ddf5
GyldenReven <simon270906@gmail.com>
2025-10-10 14:00:45 +0200

    ✨ Ajout d'un header basique

---
+++ src/components/header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+    return (
+        <header style={{ padding: '1rem', background: '#222', color: '#fff' }}>
+            <h1>My Portfolio</h1>
+        </header>
+    );
+};
+
+export default Header;
