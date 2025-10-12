16f9216940a428bf8742507d48dc2b1f930d1797
GyldenReven <simon270906@gmail.com>
2025-10-12 12:36:59 +0200

    🩹 Déplacement de la liste des projets dans project.tsx

--- src/pages/projectspage.tsx
+++ src/pages/projectspage.tsx
@@ -1,35 +1,5 @@
 import ProjectCard from "../components/projects/project_card"
-import img from "../assets/react.svg"
-import type{ Project } from "../types/project"
-import { defaultTags } from "../types/tag"
-
-let projects: Project[] = [
-    {
-        title: "Project 1",
-        description: "Description for project 1",
-        thumbnail: img,
-        icon: img,
-        tags: [defaultTags[2], defaultTags[6]]
-    },
-    {
-        title: "Project 2",
-        description: "Description for project 2",
-        thumbnail: img,
-        tags: [defaultTags[1], defaultTags[6]]
-    },
-    {
-        title: "Project 3",
-        description: "Description for project 3",
-        thumbnail: img,
-        tags: [defaultTags[0], defaultTags[8]]
-    },
-    {
-        title: "Project 4",
-        description: "Description for project 4",
-        thumbnail: img,
-        tags: [defaultTags[3], defaultTags[7]]
-    }
-];
+import { projects } from "../types/project"
 
 function ProjectsPage() {
     return <>
--- src/types/project.tsx
+++ src/types/project.tsx
@@ -1,4 +1,6 @@
 import type { Tag } from "./tag";
+import img from "../assets/react.svg"
+import { defaultTags } from "../types/tag"
 
 export type Project = {
     title: string;
@@ -7,3 +9,31 @@ export type Project = {
     icon?: string;
     tags: Tag[];
 };
+
+export const projects: Project[] = [
+    {
+        title: "Project 1",
+        description: "Description for project 1",
+        thumbnail: img,
+        icon: img,
+        tags: [defaultTags[2], defaultTags[6]]
+    },
+    {
+        title: "Project 2",
+        description: "Description for project 2",
+        thumbnail: img,
+        tags: [defaultTags[1], defaultTags[6]]
+    },
+    {
+        title: "Project 3",
+        description: "Description for project 3",
+        thumbnail: img,
+        tags: [defaultTags[0], defaultTags[8]]
+    },
+    {
+        title: "Project 4",
+        description: "Description for project 4",
+        thumbnail: img,
+        tags: [defaultTags[3], defaultTags[7]]
+    }
+];
