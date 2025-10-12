82c7bfc088344307e272ab9497d4a9dd21c68e38
GyldenReven <simon270906@gmail.com>
2025-10-12 11:45:18 +0200

    ✏️ Retrait d'un "index" inutile

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -37,7 +37,7 @@ function ProjectsPage() {
           My Projects
         </h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {projects.map((project, index) => (
+          {projects.map((project) => (
             <ProjectCard
               project={project}
             />
