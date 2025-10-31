bf1b858a3b22da1e13216e1303519e826d5397a2
GyldenReven <simon270906@gmail.com>
2025-10-31 12:33:42 +0100

    💄 Improving footer style

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import emailIcon from "../assets/email.svg";
 
 import { useTranslation } from "react-i18next";
 
@@ -6,18 +7,19 @@ const Footer: React.FC = () => {
     const { t } = useTranslation();
 
     return (
-        <footer className="text-center text-sm text-zinc-200 bg-ctp-mantle p-4 mt-8 flex justify-center items-center">
+        <footer className="text-center text-sm text-ctp-subtext0 bg-ctp-mantle p-4 mt-8 flex justify-center items-center">
             <div>
                 © 2025 {t("name")}. {t("footer.text")}
             </div>
-            <div className="ml-4">
-                {t("footer.contact")}
+            <div className="flex flex-col ml-4 align-start justify-start border-l-2 border-ctp-overlay0 pl-4">
+                <a
+                    className="hover:underline justify-start w-max"
+                    href="/contact"
+                >
+                    {t("footer.contact")}
+                </a>
                 <div className="flex justify-center items-center">
-                    <img
-                        src="/mail.svg"
-                        alt="mail icon"
-                        className="h-6 w-6 ml-4"
-                    />
+                    <img src={emailIcon} alt="mail icon" className="h-6 w-6" />
                     <a
                         href="mailto:simon270906@gmail.com"
                         target="_blank"
