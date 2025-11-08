0b2bd8b4b537fb2eaab279a60e596814a9cac743
GyldenReven <simon270906@gmail.com>
2025-11-08 19:25:00 +0100

    🩹 Fix layout in footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -12,26 +12,30 @@ const Footer: React.FC = () => {
             <div className="h-full align-center justify-center text-center mt-auto mb-auto">
                 © 2025 {t("name")}. {t("footer.text")}
             </div>
-            <div className="ml-5 mr-5 flex pt-2 mt-2 border-t-2 border-ctp-overlay0 justify-center md:border-t-0 md:mt-0 md:pt-0">
-            <div className="flex flex-col md:ml-4 align-start justify-start md:border-l-2 border-ctp-overlay0 pl-4">
-                <a
-                    className="hover:underline justify-start w-max"
-                    href="/contact"
-                >
-                    {t("footer.contact")}
-                </a>
-                <div className="flex justify-center items-center">
-                    <img src={emailIcon} alt="mail icon" className="h-6 w-6" />
+            <div className="md:w-2/3 justify-around ml-5 mr-5 flex pt-2 mt-2 border-t-2 border-ctp-overlay0 md:border-t-0 md:mt-0 md:pt-0">
+                <div className="flex flex-col md:ml-4 align-start justify-start md:border-l-2 border-ctp-overlay0 pl-4">
                     <a
-                        href="mailto:simon270906@gmail.com"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="ml-2 hover:underline"
+                        className="hover:underline justify-start w-max"
+                        href="/contact"
                     >
-                        simon270906@gmail.com
+                        {t("footer.contact")}
                     </a>
-                </div>
-                <div className="flex">
+                    <div className="flex justify-center items-center">
+                        <img
+                            src={emailIcon}
+                            alt="mail icon"
+                            className="h-6 w-6"
+                        />
+                        <a
+                            href="mailto:simon270906@gmail.com"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="ml-2 hover:underline"
+                        >
+                            simon270906@gmail.com
+                        </a>
+                    </div>
+                    <div className="flex">
                         <img
                             src={githubIcon}
                             alt="GitHub icon"
@@ -46,29 +50,29 @@ const Footer: React.FC = () => {
                             GyldenReven
                         </a>
                     </div>
-            </div>
-            <div className="h-full flex flex-col ml-4 align-start justify-start border-l-2 border-ctp-overlay0 pl-4">
-                <div className="justify-start w-max">
-                    {t("footer.pages")}
                 </div>
-                <ol className="list-disc list-inside justify-start">
-                    <li className="w-max">
-                        <a href="/" className="hover:underline">
-                            {t("header.home")}
-                        </a>
-                    </li>
-                    <li className="w-max">
-                        <a href="/projects" className="hover:underline">
-                            {t("header.projects")}
-                        </a>
-                    </li>
-                    <li className="w-max">
-                        <a href="/contact" className="hover:underline">
-                            {t("header.contact")}
-                        </a>
-                    </li>
-                </ol>
-            </div>
+                <div className="h-full flex flex-col ml-4 align-start justify-start border-l-2 border-ctp-overlay0 pl-4">
+                    <div className="justify-start w-max">
+                        {t("footer.pages")}
+                    </div>
+                    <ol className="list-disc list-inside justify-start">
+                        <li className="w-max">
+                            <a href="/" className="hover:underline">
+                                {t("header.home")}
+                            </a>
+                        </li>
+                        <li className="w-max">
+                            <a href="/projects" className="hover:underline">
+                                {t("header.projects")}
+                            </a>
+                        </li>
+                        <li className="w-max">
+                            <a href="/contact" className="hover:underline">
+                                {t("header.contact")}
+                            </a>
+                        </li>
+                    </ol>
+                </div>
             </div>
         </footer>
     );
